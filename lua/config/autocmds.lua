21750a1c4a37603d959d46d3cc79066b411b761e
shrugginG <1245280461@qq.com>
2024-09-11 11:45:46 +0800

    Change default indent to 4 spaces

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,3 +12,12 @@
 --         vim.opt_local.softtabstop = 4
 --     end,
 -- })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.expandtab = true
+    end,
+})
